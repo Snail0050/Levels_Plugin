b8f0f1e0eb6dd61e80a4b58a57f85b81390a8a25
BuildTools <unconfigured@null.spigotmc.org>
2025-12-24 20:36:49 -0600

    Added to API

--- src/main/java/com/phobia/levels/api/PlayerLevelUpEvent.java
+++ src/main/java/com/phobia/levels/api/PlayerLevelUpEvent.java
@@ -4,6 +4,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import com.phobia.levels.LevelPlugin;
+import com.phobia.levels.data.PlayerData;
+
 public class PlayerLevelUpEvent extends Event {
     private static final HandlerList HANDLERS = new HandlerList();
     private final Player player;
@@ -14,10 +17,30 @@ public class PlayerLevelUpEvent extends Event {
         this.newLevel = newLevel;
     }
 
-    public Player getPlayer() { return player; }
-    public int getNewLevel() { return newLevel; }
+    /**
+     * Helper method to fetch a player's level from the DataManager.
+     * Can be called via PlayerLevelUpEvent.getLevel(player);
+     */
+    public static int getLevel(Player player) {
+        if (player == null) return 1;
+        PlayerData data = LevelPlugin.getInstance().getPlayerDataManager().getData(player);
+        return (data != null) ? data.getLevel() : 1;
+    }
+
+    public Player getPlayer() { 
+        return player; 
+    }
+    
+    public int getNewLevel() { 
+        return newLevel; 
+    }
 
     @Override
-    public HandlerList getHandlers() { return HANDLERS; }
-    public static HandlerList getHandlerList() { return HANDLERS; }
+    public HandlerList getHandlers() { 
+        return HANDLERS; 
+    }
+    
+    public static HandlerList getHandlerList() { 
+        return HANDLERS; 
+    }
 }
